415cc82fc82024c08ab425fcc1ddea30dca660f5
MATTYGILO <chiffchaff.org@gmail.com>
2025-06-11 14:10:43 +0100

    Trying to switch to v5.4

--- src/full.cmake
+++ src/full.cmake
@@ -5,6 +5,3 @@ include(${CMAKE_CURRENT_LIST_DIR}/base.cmake)
 
 # Include ulab
 include(${CMAKE_CURRENT_LIST_DIR}/../third_party/micropython-ulab/code/micropython.cmake)
-
-# the camera driver
-include(${CMAKE_CURRENT_LIST_DIR}/micropython-camera-driver/micropython.cmake)
