06ec868201272f9d3d9bfc07890a0a909c660121
MATTYGILO <chiffchaff.org@gmail.com>
2025-11-07 15:20:52 +0000

    Use CUSTOM_BOARD_DIR

--- firmware/boards/ESP32_GENERIC_S3/mpconfigboard.cmake
+++ firmware/boards/ESP32_GENERIC_S3/mpconfigboard.cmake
@@ -1,7 +1,7 @@
 set(IDF_TARGET esp32s3)
 
 set(SDKCONFIG_DEFAULTS
-    sdkconfig.base
+    ${CUSTOM_BOARD_DIR}/../sdkconfig.base
     ${MICROPY_ESP32_DIR}/boards/sdkconfig.usb
     ${MICROPY_ESP32_DIR}/boards/sdkconfig.ble
     ${MICROPY_ESP32_DIR}/boards/sdkconfig.spiram_sx
